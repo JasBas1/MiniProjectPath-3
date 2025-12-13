861b9c8d6ce6107e9c948c471ef116c86fd00376
Jasper <basir.jasper@gmail.com>
2025-12-12 23:26:29 -0500

    Main.py

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ labels = digits.target
 #Get our training data
 X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.6, shuffle=False)
 
-def dataset_searcher(number_list, images, labels):
+def dataset_searcher(number_list,images,labels):
   #insert code that when given a list of integers, will find the labels and images
   #and put them all in numpy arrary (at the same time, as training and testing data)
   selected_images = []
@@ -94,7 +94,7 @@ print("The overall results of the KNN model is " + str(Model2_Overall_Accuracy))
 
 
 #Repeat for the MLP Classifier
-model_3 = MLPClassifier(random_state=0, solver='lbfgs')
+model_3 = MLPClassifier(random_state=0, max_iter=1000)
 model_3.fit(X_train_reshaped, y_train)
 model3_results = model_3.predict(X_test_reshaped)
 Model3_Overall_Accuracy = OverallAccuracy(model3_results, y_test)
@@ -113,28 +113,27 @@ X_train_poison = X_train + poison
 
 #Part 9-11
 #Determine the 3 models performance but with the poisoned training data X_train_poison and y_train instead of X_train and y_train
-scaler_p = MinMaxScaler()
-X_train_poison_reshaped = X_train_poison.reshape(X_train_poison.shape[0], -1)
-X_train_p_scaled = scaler_p.fit_transform(X_train_poison_reshaped)
-X_test_p_scaled = scaler_p.transform(X_test_reshaped)
-allnumbers_reshaped = allnumbers_images.reshape(allnumbers_images.shape[0], -1)
-allnumbers_p_scaled = scaler_p.transform(allnumbers_reshaped)
+# Note: for poisoned, scale the data
+X_train_poison_flat = X_train_poison.reshape(X_train_poison.shape[0], -1)
+scaler_poison = MinMaxScaler()
+X_train_poison_scaled = scaler_poison.fit_transform(X_train_poison_flat)
+X_test_scaled_poison = scaler_poison.transform(X_test_reshaped)
 
 model_1_p = GaussianNB()
-model_1_p.fit(X_train_p_scaled, y_train)
-model1_p_results = model_1_p.predict(X_test_p_scaled)
+model_1_p.fit(X_train_poison_scaled, y_train)
+model1_p_results = model_1_p.predict(X_test_scaled_poison)
 Model1_p_Accuracy = OverallAccuracy(model1_p_results, y_test)
 print("The overall results of the poisoned Gaussian model is " + str(Model1_p_Accuracy))
 
 model_2_p = KNeighborsClassifier(n_neighbors=10)
-model_2_p.fit(X_train_p_scaled, y_train)
-model2_p_results = model_2_p.predict(X_test_p_scaled)
+model_2_p.fit(X_train_poison_scaled, y_train)
+model2_p_results = model_2_p.predict(X_test_scaled_poison)
 Model2_p_Accuracy = OverallAccuracy(model2_p_results, y_test)
 print("The overall results of the poisoned KNN model is " + str(Model2_p_Accuracy))
 
-model_3_p = MLPClassifier(random_state=0, solver='lbfgs')
-model_3_p.fit(X_train_p_scaled, y_train)
-model3_p_results = model_3_p.predict(X_test_p_scaled)
+model_3_p = MLPClassifier(random_state=0, max_iter=1000)
+model_3_p.fit(X_train_poison_scaled, y_train)
+model3_p_results = model_3_p.predict(X_test_scaled_poison)
 Model3_p_Accuracy = OverallAccuracy(model3_p_results, y_test)
 print("The overall results of the poisoned MLP model is " + str(Model3_p_Accuracy))
 
@@ -146,48 +145,47 @@ print("The overall results of the poisoned MLP model is " + str(Model3_p_Accurac
 # When fitting the KernelPCA method, the input image of size 8x8 should be reshaped into 1 dimension
 # So instead of using the X_train_poison data of shape 718 (718 images) by 8 by 8, the new shape would be 718 by 64
 
-scaler = MinMaxScaler()
-X_poison_reshaped = X_train_poison.reshape(X_train_poison.shape[0], -1)
-X_poison_scaled = scaler.fit_transform(X_poison_reshaped)
+X_train_poison_flat = X_train_poison.reshape(X_train_poison.shape[0], -1)
+scaler_kpca = MinMaxScaler()
+X_train_poison_scaled = scaler_kpca.fit_transform(X_train_poison_flat)
 
 pca = KernelPCA(
     n_components=32,
     kernel="rbf",
-    gamma=1e-3,
+    gamma=0.01,
     fit_inverse_transform=True,
-    alpha=5e-3,
+    alpha=0.001,
     random_state=0,
 )
-pca.fit(X_poison_scaled)
+pca.fit(X_train_poison_scaled)
 
-X_denoised_scaled = pca.inverse_transform(pca.transform(X_poison_scaled))
-X_denoised_reshaped = scaler.inverse_transform(X_denoised_scaled)
-X_train_denoised = X_denoised_reshaped.reshape(X_train_poison.shape)
+X_denoised_scaled = pca.inverse_transform(pca.transform(X_train_poison_scaled))
+X_denoised_flat = scaler_kpca.inverse_transform(X_denoised_scaled)
+X_train_denoised = X_denoised_flat.reshape(X_train_poison.shape)
 
 
 #Part 14-15
 #Determine the 3 models performance but with the denoised training data, X_train_denoised and y_train instead of X_train_poison and y_train
 #Explain how the model performances changed after the denoising process.
-scaler_d = MinMaxScaler()
-X_train_denoised_reshaped = X_train_denoised.reshape(X_train_denoised.shape[0], -1)
-X_train_d_scaled = scaler_d.fit_transform(X_train_denoised_reshaped)
-X_test_d_scaled = scaler_d.transform(X_test_reshaped)
-allnumbers_d_scaled = scaler_d.transform(allnumbers_reshaped)
+X_train_denoised_flat = X_train_denoised.reshape(X_train_denoised.shape[0], -1)
+scaler_denoised = MinMaxScaler()
+X_train_d_scaled = scaler_denoised.fit_transform(X_train_denoised_flat)
+X_test_scaled_denoised = scaler_denoised.transform(X_test_reshaped)
 
 model_1_d = GaussianNB()
 model_1_d.fit(X_train_d_scaled, y_train)
-model1_d_results = model_1_d.predict(X_test_d_scaled)
+model1_d_results = model_1_d.predict(X_test_scaled_denoised)
 Model1_d_Accuracy = OverallAccuracy(model1_d_results, y_test)
 print("The overall results of the denoised Gaussian model is " + str(Model1_d_Accuracy))
 
 model_2_d = KNeighborsClassifier(n_neighbors=10)
 model_2_d.fit(X_train_d_scaled, y_train)
-model2_d_results = model_2_d.predict(X_test_d_scaled)
+model2_d_results = model_2_d.predict(X_test_scaled_denoised)
 Model2_d_Accuracy = OverallAccuracy(model2_d_results, y_test)
 print("The overall results of the denoised KNN model is " + str(Model2_d_Accuracy))
 
-model_3_d = MLPClassifier(random_state=0, solver='lbfgs')
+model_3_d = MLPClassifier(random_state=0, max_iter=1000)
 model_3_d.fit(X_train_d_scaled, y_train)
-model3_d_results = model_3_d.predict(X_test_d_scaled)
+model3_d_results = model_3_d.predict(X_test_scaled_denoised)
 Model3_d_Accuracy = OverallAccuracy(model3_d_results, y_test)
 print("The overall results of the denoised MLP model is " + str(Model3_d_Accuracy))
